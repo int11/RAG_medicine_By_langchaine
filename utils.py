dfd610e4b204543ff65773f2be62671a325f2f22
LimHHSS <LimHHSS>
2024-06-05 23:00:19 +0900

    # 사이드바에 소스코드로 바로가기 버튼 추가 # 이모지 추가로 UI 조금 덜 심심하게 수정 # 출력 모델을 gpt-4o로 수정하고 api-key 입력 후 나타나는 Model Select box 도 0번째 인덱스인 gpt-3-turbo가 아닌, 18번째 인덱스인 gpt-4o로 기본값 선택되게 수정(길게썼지만 사실 뭣도 아닌 수정)

--- utils.py
+++ utils.py
@@ -3,22 +3,27 @@ import openai
 from datetime import datetime
 import streamlit as st
  
-def configure_openai():
+st.sidebar.markdown('[![](https://img.shields.io/badge/7조_소스코드_보러가기-red?logo=github)](https://github.com/int11/langchaine_medicine/blob/main/main.py)')
+
+ 
+def configure_openai():  
     openai_api_key = st.sidebar.text_input(
-        label="OpenAI API Key",
+        label="🗝️ OpenAI API Key",
         type="password",
         value=st.session_state['OPENAI_API_KEY'] if 'OPENAI_API_KEY' in st.session_state else '',
         placeholder="sk-..."
         )
+    
+    
     if openai_api_key:
         st.session_state['OPENAI_API_KEY'] = openai_api_key
         os.environ['OPENAI_API_KEY'] = openai_api_key
     else:
-        st.error("Please add your OpenAI API key to continue.")
-        st.info("Obtain your key from this link: https://platform.openai.com/account/api-keys")
+        st.warning("🔑 API 키를 추가하고 진행해주세요!")
+        st.info("🔗 링크를 통해 API 키를 발급받을 수 있습니다. https://platform.openai.com/account/api-keys")
         st.stop()
 
-    model = "gpt-3.5-turbo"
+    model = "gpt-4o"
     try:
         client = openai.OpenAI()
         available_models = [{"id": i.id, "created":datetime.fromtimestamp(i.created)} for i in client.models.list() if str(i.id).startswith("gpt")]
@@ -26,9 +31,9 @@ def configure_openai():
         available_models = [i["id"] for i in available_models]
 
         model = st.sidebar.selectbox(
-            label="Model",
+            label="✅ Model 선택",
             options=available_models,
-            index=available_models.index(st.session_state['OPENAI_MODEL']) if 'OPENAI_MODEL' in st.session_state else 0
+            index=available_models.index(st.session_state['OPENAI_MODEL']) if 'OPENAI_MODEL' in st.session_state else 18  # 인덱스 18은 gpt-4o  
         )
         st.session_state['OPENAI_MODEL'] = model
     except openai.AuthenticationError as e:
@@ -36,6 +41,6 @@ def configure_openai():
         st.stop()
     except Exception as e:
         print(e)
-        st.error("Something went wrong. Please try again later.")
+        st.error("문제가 발생한거 같아요, 잠시 후 다시 시도해보시겠어요?")
         st.stop()
     return model
